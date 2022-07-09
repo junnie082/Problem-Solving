e0f015486bd8bc58160f5f9816ea1ebe3df83837
junnie082 <junnie082@gmail.com>
2022-07-09 22:51:24 +0900

    백준 1284번. 집 주소

--- 1284.c
+++ 1284.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-// #include <stdlib.h>
 #include <string.h>
 
 int width(char *str, int len){
@@ -19,18 +18,11 @@ int main()
 {
 	int i = 0;
 	char str[5];
-	//register int *p = (int *)malloc(sizeof(int) * 1000);
 
 	while(1) {
 		scanf("%s", str);
 		if(*str == '0') break;
-	//	*(p+(i++)) = width(str, strlen(str));
 		printf("%d\n", width(str, strlen(str)));
 	}
-
-	// for(int j=0; j<i; j++) {
-	// 	printf("%d\n", *(p+j));
-	// }
 	return 0;
-	//free(p);
 }
